6aadf7ad8d1e32d8baa8362cb89cab412574aea2
raina96 <welcomerain4@gmail.com>
2024-05-02 15:23:15 +0900

    vue-app 추가

---
+++ vue-app/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="position-relative">
+    <div id="fullpage">
+      <div id="full1" class="section">Section 1</div>
+      <div class="section full2">
+        <img  src="./assets/images/section2Title.png" alt="titleImg" class="titleImg">
+        <img src="./assets/images/section2Subbg.png" alt="초대장" class="subBg">
+        <form action="emailReg" method="POST" id="section2Form">
+          <input type="text" placeholder="이메일을 입력해주세요." name="email" required>
+          <button type="submit" class="btn">인증</button>
+        </form>
+
+      </div>
+      <div class="section">Section 3</div>
+      <div class="section">Section 4</div>
+    </div>
+  </div>
+
+  <!--  <router-view/>-->
+</template>
+
+<script>
+  export default {
+    mounted() {
+      new fullpage('#fullpage', {
+        sectionSelector: '.section',
+        scrollOverflow: true,
+        navigation: true,
+        scrollingSpeed: 1000,
+      });
+    }
+  }
+</script>
+
+<style>
+  .section {
+    background-image: url('assets/images/bgImg.jpg');
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  /*Section2*/
+  .titleImg {
+    position: absolute;
+    width: 40rem;
+    top: 3%;
+    left: 3%;
+    z-index: 1;
+  }
+  .subBg {
+    position: absolute;
+    width: 100rem;
+    z-index: 0;
+    top: 5rem;
+    left: 6rem;
+  }
+  #section2Form {
+    position: absolute;
+    left: 30rem;
+    top: 20rem;
+    z-index: 2;
+  }
+
+
+  /*fullpage nav 재정의*/
+
+  #fp-nav.fp-right {
+    right: 5%; /*네비바 오른쪽 여백*/
+  }
+  #fp-nav ul li {
+      margin: 5rem;
+  }
+  #fp-nav ul li a span {
+    background: unset;
+  }
+
+  #fp-nav ul li a {
+    width: 4rem; /* 내비게이션 아이템의 너비 설정 */
+    height: 4rem; /* 내비게이션 아이템의 높이 설정 */
+    background-image: url('https://item.kakaocdn.net/do/4155f71e3350f0ee40ffde6dcdcb2804d0bbab1214a29e381afae56101ded106'); /* 배경 이미지 설정 */
+    background-size: cover; /* 배경 이미지를 아이템에 맞게 조정 */
+  }
+
+  #fp-nav ul li a.active {
+    width: 4rem; /* 내비게이션 아이템의 너비 설정 */
+    height: 4rem; /* 내비게이션 아이템의 높이 설정 */
+    background-image: url('https://item.kakaocdn.net/do/4155f71e3350f0ee40ffde6dcdcb2804f604e7b0e6900f9ac53a43965300eb9a'); /* 배경 이미지 설정 */
+    background-size: cover; /* 배경 이미지를 아이템에 맞게 조정 */
+  }
+</style>
---
+++ vue-app/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <full-page ref="fullpage" :options="options" id="fullpage">
+        <div class="section">
+            Section 1
+        </div>
+        <div class="section">
+            Section 2
+        </div>
+    </full-page>
+</template>
+
+<script>
+
+</script>
